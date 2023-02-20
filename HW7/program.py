deb3d9dc7bd54c35dcf5fea6f2c3526e39c124e7
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-02-20 20:31:42 +0300

    HW 7 done right

--- HW7/program.py
+++ HW7/program.py
@@ -35,8 +35,22 @@
 #     print('Парам пам-пам')
 # else:
 #     print('Пам парам')
+# var 2
+stroka1 = 'пара-ра-рам рам-пaм-папам па-ра-па-дам'
+vowels = ['а', 'е', 'ё', 'и', 'й', 'о', 'у', 'ы', 'э', 'ю', 'я']
+phrases = stroka1.split()
+if len(phrases) < 2:
+    print('Количество фраз должно быть больше одной!')
+else:
+    countVowels = []
 
+    for i in phrases:
+        countVowels.append(len([x for x in i if x.lower() in vowels]))
 
+    if countVowels.count(countVowels[0]) == len(countVowels):
+        print('Парам пам-пам')
+    else:
+        print('Пам парам')
 
 
 # Задача 36:
@@ -68,13 +82,20 @@
 #
 #
 # print_operation_table(lambda x, y: x * y)
-# var2 # не смог убрать распечатку списка, надо доп изучение
-# def print_operation_table(operation, num_rows=6, num_columns=6):
-#     return [print(*print_rows(operation, i, num_columns)) for i in range(1, num_rows + 1)]
-#
-#
-# def print_rows(f, i, num_columns):
-#     return [print(f(k, i), end=' ') for k in range(1, num_columns + 1)]
-#
-#
-# print_operation_table(lambda x, y: x * y)
+
+
+# var 3
+
+def print_operation_table(operation, num_rows=4, num_columns=4):
+    if num_rows < 2 or num_columns < 2:
+        print('ОШИБКА! Размерности таблицы должны быть больше 2!')
+    else:
+        print('	'.join([str(i) for i in range(1, num_columns + 1)]))
+        for i in range(2, num_rows + 1):
+            print(i, end='	')
+            for j in range(2, num_columns + 1):
+                print(operation(i, j), end='	')
+            print()
+
+
+print_operation_table(lambda x, y: x * y)
